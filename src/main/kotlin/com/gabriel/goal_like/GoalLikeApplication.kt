bd37cf054bf183d8cee2752cf6b052b9e71610a2
Anebe <46246218+Anebe@users.noreply.github.com>
2026-02-24 12:56:51 -0300

    Initial commit

---
+++ src/main/kotlin/com/gabriel/goal_like/GoalLikeApplication.kt
@@ -0,0 +1,11 @@
+package com.gabriel.goal_like
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoalLikeApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoalLikeApplication>(*args)
+}
---
+++ src/test/kotlin/com/gabriel/goal_like/GoalLikeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gabriel.goal_like
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoalLikeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
